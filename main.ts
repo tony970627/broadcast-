f1f5e34e6752dfa56249669047a762b377bc38d6
tony970627 <104812234+tony970627@users.noreply.github.com>
2026-01-01 19:59:34 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,17 @@
-enum RadioMessage {
-    message1 = 49434
-}
-radio.setGroup(226)
+radio.setGroup(7)
 radio.setTransmitPower(7)
-radio.sendString("check-s")
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     if (GHBit.Rocker(GHBit.enRocker.Up)) {
         radio.sendString("Forward")
-    }
-})
-basic.forever(function () {
-    if (GHBit.Rocker(GHBit.enRocker.Left)) {
+    } else if (GHBit.Rocker(GHBit.enRocker.Down)) {
+        radio.sendString("Backward")
+    } else if (GHBit.Rocker(GHBit.enRocker.Left)) {
         radio.sendString("Left")
-    }
-})
-basic.forever(function () {
-    if (GHBit.Rocker(GHBit.enRocker.Left)) {
-        radio.sendString("Left")
-    }
-})
-basic.forever(function () {
-    if (GHBit.Rocker(GHBit.enRocker.Right)) {
+    } else if (GHBit.Rocker(GHBit.enRocker.Right)) {
         radio.sendString("Right")
-    }
-})
-basic.forever(function () {
-    if (GHBit.Rocker(GHBit.enRocker.Nostate)) {
+    } else if (GHBit.Rocker(GHBit.enRocker.Nostate)) {
         radio.sendString("Stop")
     }
+    basic.pause(100)
 })
